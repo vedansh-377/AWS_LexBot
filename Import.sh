70d71b85a13c419da11fdb64aefaa72ce4a77bc9
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2024-01-29 22:13:26 +0530

    changes

---
+++ Import.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+pip install --upgrade pip setuptools
+pip install -r requirements.txt -t lib
+
+echo "Zipping deployment package..."
+cd lib || { echo "Error: Could not change to 'lib' directory"; exit 1; }
+zip -r9 ../deployment_package.zip .
+cd ..
+
+echo "Checking if Lambda function exists..."
+FUNCTION_NAME="github-to-lambda-demo"
+FUNCTION_EXISTS=$(aws lambda list-functions --query "Functions[?FunctionName=='$FUNCTION_NAME'].FunctionName" --output text --region us-east-1 || true)
+
+if [ -z "$FUNCTION_EXISTS" ]; then
+  echo "Creating new Lambda function..."
+  aws lambda create-function --function-name "$FUNCTION_NAME" --runtime python3.11 --handler lambda.handler --role arn:aws:iam::003261238302:role/Lambda --zip-file fileb://deployment_package.zip --region us-east-1 --timeout 600
+else
+  echo "Updating existing Lambda function..."
+  aws lambda update-function-code --function-name "$FUNCTION_NAME" --zip-file fileb://deployment_package.zip --region us-east-1
+fi
+
+echo "Invoking Lambda function..."
+aws lambda invoke --function-name "$FUNCTION_NAME" --payload '{}' output.txt --region us-east-1
+cat output.txt  # Display the Lambda output
+
+echo "DONE!!"
