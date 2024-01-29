8572d14bac873472e589525261ccbc96f49f8729
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2024-01-30 01:40:03 +0530

    changes

--- Import.sh
+++ Import.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
-pip install --upgrade pip setuptools
-pip install -r requirements.txt -t lib
+# Your import logic to migrate and import data from S3
+# Example: Migrate data from S3 bucket to another AWS account
 
-echo "Zipping deployment package..."
-(cd lib && zip -r9 ../deployment_package.zip .)
-zip -g deployment_package.zip lambda.py
+DESTINATION_BUCKET="my-lexv2-import-bucket"
 
-echo "Checking if Lambda function exists..."
-FUNCTION_NAME="github-to-lambda-demo"
-FUNCTION_EXISTS=$(aws lambda list-functions --query "Functions[?FunctionName=='$FUNCTION_NAME'].FunctionName" --output text --region us-east-1s)
 
-if [ -z "$FUNCTION_EXISTS" ]; then
-  echo "Creating new Lambda function..."
-  aws lambda create-function --function-name "$FUNCTION_NAME" --runtime python3.11 --handler lambda.handler --role arn:aws:iam::003261238302:role/Lambda --zip-file fileb://deployment_package.zip --region us-east-1 --timeout 900
-else
-  echo "Updating existing Lambda function..."
-  aws lambda update-function-code --function-name "$FUNCTION_NAME" --zip-file fileb://deployment_package.zip --region us-east-1
-fi
-sleep 20
-echo "Invoking Lambda function..."
-aws lambda invoke --function-name "$FUNCTION_NAME" --payload '{}' output.txt --region us-east-1
-cat output.txt  # Display the Lambda output
+# Create the destination S3 bucket if it doesn't exist
+aws s3api create-bucket --bucket $DESTINATION_BUCKET --region <destination_region>
 
-echo "DONE!!"
+# Your migration logic here
+
+# Import data to the destination S3 bucket
+aws s3 cp export_data.zip s3://$DESTINATION_BUCKET/
