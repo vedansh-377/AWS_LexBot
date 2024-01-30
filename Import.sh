f217955153bd7b9213e086bd4cc4ad204f00a4fb
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2024-01-30 12:53:31 +0530

    changes

--- Import.sh
+++ Import.sh
@@ -15,3 +15,27 @@ aws s3api create-bucket --bucket $DESTINATION_BUCKET --region $DESTINATION_REGIO
 
 # Import data to the destination S3 bucket
 aws s3 cp exported_bot.zip s3://$DESTINATION_BUCKET/
+
+echo "Installing dependencies..."
+pip install --upgrade pip setuptools
+pip install -r requirements.txt -t lib
+
+echo "Zipping deployment package..."
+(cd lib && zip -r9 ../deployment_package_1.zip .)
+zip -g deployment_package.zip IamImport.py
+
+echo "Checking if Lambda function exists..."
+FUNCTION_NAME="github-to-lambda-demo"
+FUNCTION_EXISTS=$(aws lambda list-functions --query "Functions[?FunctionName=='$FUNCTION_NAME'].FunctionName" --output text --region us-east-1)
+
+if [ -z "$FUNCTION_EXISTS" ]; then
+  echo "Creating new Lambda function..."
+  aws lambda create-function --function-name "$FUNCTION_NAME" --runtime python3.11 --handler IamImport.handler --role arn:aws:iam::526222510576:role/IamImport --zip-file fileb://deployment_package_1.zip --region us-east-1 --timeout 900
+else
+  echo "Updating existing Lambda function..."
+  aws lambda update-function-code --function-name "$FUNCTION_NAME" --zip-file fileb://deployment_package_1.zip --region us-east-1
+fi
+sleep 20
+echo "Invoking Lambda function..."
+aws lambda invoke --function-name "$FUNCTION_NAME" --payload '{}' output.txt --region us-east-1
+cat output.txt  # Display the Lambda output
