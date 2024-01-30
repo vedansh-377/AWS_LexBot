63a574576d8c00c68c8a51f55c3b6c9bb554d3b3
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2024-01-30 11:29:23 +0530

    changes

--- Import.sh
+++ Import.sh
@@ -3,10 +3,13 @@
 # Your import logic to migrate and import data from S3
 # Example: Migrate data from S3 bucket to another AWS account
 
+# Set the destination bucket name and region
 DESTINATION_BUCKET="my-lexv2-import-bucket"
+DESTINATION_REGION="us-east-1"
+
 
 # Create the destination S3 bucket if it doesn't exist
-aws s3api create-bucket --bucket $DESTINATION_BUCKET --region <destination_region>
+aws s3api create-bucket --bucket $DESTINATION_BUCKET --region $DESTINATION_REGION
 
 # Your migration logic here
 
