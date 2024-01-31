286a3d9da4afaecbd87d3d08072ee52997eb9f41
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2024-02-01 01:34:56 +0530

    Trial

--- Import.sh
+++ Import.sh
@@ -8,7 +8,7 @@ DESTINATION_BUCKET="my-lexv2-import-bucket"
 DESTINATION_REGION="us-east-1"
 
 # Check if any zip file exists in the /tmp directory
-EXPORTED_MODEL_FILENAME=$(ls -t /tmp/*.zip | head -n 1)
+# EXPORTED_MODEL_FILENAME=$(ls -t /tmp/*.zip | head -n 1)
 if [ -n "$EXPORTED_MODEL_FILENAME" ]; then
     echo "Using existing exported model: $EXPORTED_MODEL_FILENAME"
 else
